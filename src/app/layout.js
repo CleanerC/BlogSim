080f6b1a6fe3d446522bf1de8e9db6a4ad6c4248
cleanerc <xjw031201@gmail.com>
2024-12-31 10:22:29 -0500

    fixed login button, added create blog button

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,12 +4,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <button className="fixed top-4 right-4 px-4 py-2 hover:bg-blue-500">
+        <span className="absolute top-4 right-4 px-4 py-2 cursor-pointer hover:text-blue-800 hover:font-extrabold">
           Login
-        </button>
+        </span>
         <div className="w-full p-4 border-b-4 border-gray-700">
           <h1 className="text-4xl font-bold tracking-wider">MYBLOG</h1>
         </div>
+
+        {/* create button */}
+        <button className="fixed rounded-full p-6 border-4 border-black bottom-4 right-4 shadow-xl cursor-pointer hover:scale-105 transition-all hover:font-bold">
+          Create a Blog
+        </button>
+
         {children}
       </body>
     </html>
